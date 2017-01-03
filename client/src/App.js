9ceba4c641ffa52a893237638cbe47957833b9b6
Wesley Jones <wjones127@gmail.com>
2017-01-03 18:57:50 -0500

    starting header component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ApiUtils from './utils/ApiUtils';
 import Login from './components/Login';
+import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 
 class App extends Component {
@@ -16,39 +17,41 @@ class App extends Component {
   handleLogin = (user, token) => {
     this.setState({ user: { name: user.name }, error: null });
     localStorage.setItem("sessionToken", token);
-    this.getUser();
+    this.forceUpdate();
+    return;
   }
 
-    getUser = () => {
-        let _this = this;
-        let url = 'http://localhost:8080/api/user/';
-        let settings = {
-            method: 'GET'
-        };
+  handleLogout = () => {
+    let _this = this;
+    let url = 'http://localhost:8080/api/logout/';
+    let settings = {
+      method: 'POST'
+    };
 
-        ApiUtils.fetchResponse(url, settings)
-            .then(json => {
-                _this.setState({user: {name: json.first_name}});
-            })
-            .catch(error => {
-                var key;
-                let errors = '';
-                for (key in error) {
-                    if (error.hasOwnProperty(key)) {
-                        errors = errors + key + ' = ' + error[key];
-                    }
-                }
-                _this.setState({ error: errors });
-            });
-    }
+    return ApiUtils.fetchResponse(url, settings)
+      .then(json => {
+        _this.setState({ user: null, error: null });
+        localStorage.removeItem("sessionToken");
+      })
+      .catch(error => {
+        var key;
+        let errors = '';
+        for (key in error) {
+          if (error.hasOwnProperty(key)) {
+            errors = errors + key + ' = ' + error[key];
+          }
+        }
+        _this.setState({ error: errors });
+      });
+
+  }
+
 
   render() {
     return (
       <div className="App">
         {this.state.user &&
-          <div className="App-header">
-            <h2>Welcome {this.state.user.name} - {this.state.user.password}</h2>
-          </div>
+          <Header logout={this.handleLogout} />
         }
         {this.state.user && <Dashboard />}
         {!this.state.user &&
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
+import ApiUtils from '../utils/ApiUtils';
 
 class Header extends Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.getUser();
+    }
+
+  getUser = () => {
+    let _this = this;
+    let url = 'http://localhost:8080/api/user/';
+    let settings = {
+      method: 'GET'
+    };
+
+    ApiUtils.fetchResponse(url, settings)
+      .then(json => {
+        _this.setState({ user: { name: json.first_name + ' ' + json.last_name } });
+      })
+      .catch(error => {
+        var key;
+        let errors = '';
+        for (key in error) {
+          if (error.hasOwnProperty(key)) {
+            errors = errors + key + ' = ' + error[key];
+          }
+        }
+        _this.setState({ error: errors });
+      });
+  }
+    
     render() {
         return (
             <div className="App-header">
@@ -8,7 +41,7 @@ class Header extends Component {
                     <h2>Welcome {this.state.user.name}</h2>
                 </div>
                 <div>
-                    <h6>Logout</h6>
+                    <h6 onClick={() => this.props.Logout}>Logout</h6>
                 </div>
             </div>
         );
