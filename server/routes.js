414eef9e52fb79894a353f55965f414b66b27f4e
Wesley Jones <wjones127@gmail.com>
2017-01-04 16:23:59 -0500

    async api calls working

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var async = require('async');
 
 const apiBase = 'https://api.robinhood.com/';
 var router = express.Router();
@@ -22,28 +23,66 @@ router.route('/login')
 router.post('/logout', function (req, res) {
 	var url = apiBase + 'api-token-logout/';
 	var headers = { 'authorization': req.header('authorization') };
-	request({
-		headers: headers,
-		uri: url,
-		method: 'POST'
-	},
+	request({ headers: headers, uri: url, method: 'POST' },
 		function (error, response, body) {
 			res.status(response.statusCode).json(response.body);
-		})
-
+		});
 });
 
 router.get('/user', function (req, res) {
 	var url = apiBase + 'user/';
 	var headers = { 'authorization': req.header('authorization') };
-	request({
-		headers: headers,
-		uri: url,
-		method: 'GET'
-	},
+	request({ headers: headers, uri: url, method: 'GET' },
 		function (error, response, body) {
-			res.status(response.statusCode).json(JSON.parse(response.body));
-		})
+			res.status(response.statusCode).json(JSON.parse(body));
+		});
+});
+
+router.get('/headerInfo', function (req, res) {
+	async.parallel([
+		// get user info
+		function (callback) {
+			var url = apiBase + 'user/';
+			var headers = { 'authorization': req.header('authorization') };
+			request({ headers: headers, uri: url, method: 'GET' },
+				function (error, response, body) {
+					if (error) { console.log(error); callback(true); return; }
+					obj = JSON.parse(body);
+					callback(false, obj);
+				});
+		},
+		// get accounts info
+		function (callback) {
+			var url = apiBase + 'accounts/';
+			var headers = { 'authorization': req.header('authorization') };
+			request({ headers: headers, uri: url, method: 'GET' },
+				function (error, response, body) {
+					if (error) { console.log(error); callback(true); return; }
+					obj = JSON.parse(body);
+					callback(false, obj);
+				});
+
+		}
+	],
+		function (err, results) {
+			if (err) { console.log(err); res.send(500, err); return; }
+			res.send({
+				user: {
+					id: results[0].id,
+					username: results[0].username,
+					first_name: results[0].first_name,
+					last_name: results[0].last_name
+				},
+				accounts: {
+					account_number: results[1].results[0].account_number,
+					cash: results[1].results[0].cash,
+					cash_available_for_withdrawal: results[1].results[0].cash_available_for_withdrawal,
+					unclear_deposits: results[1].results[0].unclear_deposits,
+					unsettled_funds: results[1].results[0].unsettled_funds,
+					buying_power: results[1].results[0].buying_power
+				}
+			});
+		});
 });
 
 router.get('/', function (req, res) {
